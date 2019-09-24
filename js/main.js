079f08c231f4b73ba860e6f37a6f7f3c3da78fa9
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-24 16:41:29 +0200

    Zadanie: zrobione

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-function mojaFunkcja(){
-    var imie = prompt("Podaj imię");
-    var nazwisko = prompt("Podaj nazwisko");
-    var mail = prompt("Podaj mail");
-    if(imie){
-        document.getElementById("#name").value = imie;
-    }else{
-        document.getElementById("#name").value = "Bład";
-    }
-    if(nazwisko){
-        document.getElementById("#surname").value = nazwisko;
-    }else{
-        document.getElementById("#surname").value = "Bład";
-    }
-    if(mail){
-        document.getElementById("#email").value = mail;
-    }else{
-        document.getElementById("#email").value = "Bład";
-    }
+var imie = prompt("Podaj imię");
+var nazwisko = prompt("Podaj nazwisko");
+var mail = prompt("Podaj mail");
+
+if(imie){
+    document.getElementById("name").value = imie;
+}else{
+    document.getElementById("name").value = "Bład";
 }
+if(nazwisko){
+    document.getElementById("surname").value = nazwisko;
+}else{
+    document.getElementById("surname").value = "Bład";
+}
+if(mail){
+    document.getElementById("email").value = mail;
+}else{
+    document.getElementById("email").value = "Bład";
+}
+
+
 
 
